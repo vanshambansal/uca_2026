b045ed9099b2a0c4e1f759be44a8eb71051cd8fa
Vansham Bansal <vanshambansal1210@gmail.com>
2026-07-17 01:48:27 +0530

    reversed a string

--- c_programming/Week-2(Assignment)/reverseString.c
+++ c_programming/Week-2(Assignment)/reverseString.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 
+void reverse_string(char *str) {
+
+    int len = strlen(str);
+    if(str[len-1] == '\n')
+    {
+        str[len-1] = '\0';
+        len--;
+    }
+
+    for(int i=0;i<len/2;i++){
+        char temp = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = temp;
+    }
+    printf("%s", str);
+
+}
+
+
 int main(){
 
     char str[100];
     fgets(str, sizeof(str), stdin);
 
-    int len = strlen(str);
-    printf("%d", len);
-
+    reverse_string(str);
 
     return 0;
 }
